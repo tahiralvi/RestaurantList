85c234536d71f11dbb2cd063714ec04a62d626c7
tahiralvi <engtahiralvi@gmail.com>
2025-09-10 00:56:14 -0700

    Adding Restaurants, Restaurants detail, Dishes and added some more dishes for each retaurants
    Adding Restaurants, Restaurants detail, Dishes and added some more dishes for each restaurants

---
+++ RestaurantList/Controllers/RestaurantList.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantList.Data;
+
+namespace RestaurantList.Controllers
+{
+    public class RestaurantList : Controller
+    {
+        private readonly RestaurantListContext _context;
+
+        public RestaurantList(RestaurantListContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Restaurants.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            var restaurant = await _context.Restaurants
+            .Include(rd => rd.RestaurantDishes)
+            .ThenInclude(d => d.Dish)
+            .FirstOrDefaultAsync(x => x.Id == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            return View(restaurant);
+        }
+    }
+}
