8c761f8daa7f7a8792e0d866400b1b942c2734c2
Tahir Alvi <engtahiralvi@gmail.com>
2026-02-14 17:51:16 +0500

    Update the controller for Create and Edit the Resturant

--- RestaurantList/Controllers/RestaurantList.cs
+++ RestaurantList/Controllers/RestaurantList.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RestaurantList.Data;
+using RestaurantList.Models;
 
 namespace RestaurantList.Controllers
 {
@@ -35,6 +36,80 @@ namespace RestaurantList.Controllers
                 return NotFound();
             }
             return View(restaurant);
+        }
+
+        // GET: RestaurantList/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RestaurantList/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ImageUrl,Address")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(restaurant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(restaurant);
+        }
+
+        // GET: RestaurantList/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            return View(restaurant);
+        }
+
+        // POST: RestaurantList/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ImageUrl,Address")] Restaurant restaurant)
+        {
+            if (id != restaurant.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(restaurant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RestaurantExists(restaurant.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(restaurant);
+        }
+
+        private bool RestaurantExists(int id)
+        {
+            return _context.Restaurants.Any(e => e.Id == id);
         }
     }
 }
